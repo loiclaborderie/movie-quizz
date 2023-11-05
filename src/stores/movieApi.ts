fc5261afc30464354b4d9273d8acec5df13696bc
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2023-11-05 14:25:41 +0700

    chore: add fetch from store

--- src/stores/movieApi.ts
+++ src/stores/movieApi.ts
@@ -1,25 +1,26 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { useFetch } from '@vueuse/core'
-
-const { isFetching, error, data } = await useFetch(url)
+const apiKey = import.meta.env.VITE_TMDB_APIKEY
 
 export const useMovieApiStore = defineStore('movieApi', () => {
-  function searchMovie(movieName: string) {
-    let url: string = 'https://api.themoviedb.org/3/search/movie?query=machine&api_key='
-    url = url + apikey
+  async function searchMovie(movieName: string) {
+    console.log(apiKey)
+    const url: string = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieName}`
+    const { isFetching, error, data } = await useFetch(url).get().json()
+    return { isFetching, error, data }
   }
-  function getMovieById(movieId: number) {
-    const url: string = `https://api.themoviedb.org/3/movie/${movieId}?api_key=`
-    return url
-    // url = url + apikey
+  async function getMovieById(movieId: number) {
+    const url: string = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
+    const { isFetching, error, data } = await useFetch(url).get().json()
+    return { isFetching, error, data }
   }
-  function getRandomMovie() {
+  async function getRandomMovie() {
     const page = Math.floor(Math.random() * 50)
-    console.log(page)
-    const url = `https://api.themoviedb.org/3/discover/movie?&language=fr_FR&include_adult=false&page=${page}&sort_by=popularity.desc&api_key=`
-    return url
-    // url = url + apikey
+    console.log(page, apiKey)
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr_FR&include_adult=false&page=${page}&sort_by=popularity.desc`
+    const { isFetching, error, data } = await useFetch(url).get().json()
+    return { isFetching, error, data }
   }
 
   return { searchMovie, getRandomMovie, getMovieById }
