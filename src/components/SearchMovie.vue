53600e137a3e0bb154a0c089ce1859b487d7bfbf
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2023-11-12 11:09:15 +0700

    chore: rearranged movie search logic

--- src/components/SearchMovie.vue
+++ src/components/SearchMovie.vue
@@ -4,157 +4,262 @@ import { movieGuessFinderStore } from '@/stores/movieGuessFinder'
 import type MovieOverview from '@/types/MovieOverview'
 import { ref } from 'vue'
 import SearchSuggestions from '@/components/SearchSuggestions.vue'
+import { debounce } from '@/helpers/debounce.ts'
 
 const movieDataStore = useMovieApiStore()
 const movieFinderStore = movieGuessFinderStore()
+const emit = defineEmits(['search', 'found'])
 
 const searchTerm = ref('')
+const searching = ref(false)
 let searchResults = ref<null | MovieOverview[]>(null)
 
 async function fireSearch() {
+  searching.value = true
   const foundData = await movieDataStore.searchMovie(searchTerm.value)
   searchResults.value = foundData.data.value.results
   console.log(searchResults.value)
 }
 
-function compareMovies(movieId: number) {
-  movieFinderStore.compareMovie(movieId)
+const debouncedFireSearch = debounce(fireSearch, 800)
+
+async function compareMovies(movieId: number) {
+  searching.value = false
+  searchResults.value = null
+  searchTerm.value = ''
+  const movieMatch = movieFinderStore.compareMovie(movieId)
+  if (!movieMatch) {
+    const result = await movieFinderStore.getHints(movieId)
+    if (result) {
+      const { cluesFound, hintsFound } = result
+      emit('search')
+      console.log('IMPORTANT TEST FINAL', cluesFound, hintsFound.value)
+    }
+  } else {
+    emit('found')
+  }
 }
 </script>
 
 <template>
-  <div class="input__container">
-    <div class="shadow__input"></div>
-    <button @click="fireSearch" class="input__button__shadow">
-      <svg
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        height="20px"
-        width="20px"
-      >
-        <path
-          d="M4 9a5 5 0 1110 0A5 5 0 014 9zm5-7a7 7 0 104.2 12.6.999.999 0 00.093.107l3 3a1 1 0 001.414-1.414l-3-3a.999.999 0 00-.107-.093A7 7 0 009 2z"
-          fill-rule="evenodd"
-          fill="#17202A"
-        ></path>
-      </svg>
-    </button>
-    <input
-      v-model="searchTerm"
-      type="text"
-      name="text"
-      class="input__search"
-      placeholder="Guess the movie"
-    />
-  </div>
-  <Suspense>
-    <div class="resultsSuggested" v-if="searchResults?.length">
-      <div class="suggested" v-for="searchResult in searchResults" :key="searchResult.id">
-        <SearchSuggestions :movie="searchResult" @select="compareMovies" />
-      </div>
+  <div class="searchAndResults">
+    <div class="search-bar">
+      <input
+        placeholder="Guess the movie"
+        type="search"
+        name="search"
+        autocomplete="off"
+        required
+        @input="debouncedFireSearch"
+        v-model="searchTerm"
+      />
+      <button class="search-btn" @click="fireSearch">
+        <span>Search</span>
+      </button>
     </div>
-  </Suspense>
+    <Suspense>
+      <template #default>
+        <div class="resultsSuggested" v-if="searching && searchResults?.length">
+          <div class="suggested" v-for="searchResult in searchResults" :key="searchResult.id">
+            <SearchSuggestions :movie="searchResult" @select="compareMovies" />
+          </div>
+        </div>
+        <div
+          class="resultsSuggested"
+          v-else-if="searching && searchResults && searchResults.length == 0 && searchTerm.length"
+        >
+          <div class="suggested empty">No result found...</div>
+        </div>
+      </template>
+      <template #fallback>
+        <div class="resultsSuggested">
+          <div class="suggested empty">Loading...</div>
+        </div>
+      </template>
+    </Suspense>
+  </div>
 </template>
 
 <style scoped>
-.input__container {
-  position: relative;
-  background: rgba(255, 255, 255, 0.664);
-  padding: 10px 15px;
-  display: -webkit-box;
-  display: -ms-flexbox;
+:root {
+  font-size: calc(16px + (24 - 16) * (100vw - 320px) / (1920 - 320));
+}
+body,
+button,
+input {
+  font:
+    1em Hind,
+    sans-serif;
+  line-height: 1.5em;
+}
+body,
+input {
+  color: #171717;
+}
+body,
+.search-bar {
   display: flex;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  justify-content: center;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-  gap: 5px;
-  border-radius: 22px;
-  max-width: 300px;
-  -webkit-transition: -webkit-transform 400ms;
-  transition: -webkit-transform 400ms;
-  transition: transform 400ms;
-  transition:
-    transform 400ms,
-    -webkit-transform 400ms;
-  -webkit-transform-style: preserve-3d;
-  transform-style: preserve-3d;
-  -webkit-transform: rotateX(15deg) rotateY(-20deg);
-  transform: rotateX(15deg) rotateY(-20deg);
-  -webkit-perspective: 500px;
-  perspective: 500px;
 }
-
-.shadow__input {
-  content: '';
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  left: 0;
-  bottom: 0;
-  z-index: -1;
-  -webkit-filter: blur(30px);
-  filter: blur(30px);
-  border-radius: 20px;
-  background-color: #999cff;
-  background-image: radial-gradient(at 85% 51%, hsla(60, 60%, 61%, 1) 0px, transparent 50%),
-    radial-gradient(at 74% 68%, hsla(235, 69%, 77%, 1) 0px, transparent 50%),
-    radial-gradient(at 64% 79%, hsla(284, 72%, 73%, 1) 0px, transparent 50%),
-    radial-gradient(at 75% 16%, hsla(283, 60%, 72%, 1) 0px, transparent 50%),
-    radial-gradient(at 90% 65%, hsla(153, 70%, 64%, 1) 0px, transparent 50%),
-    radial-gradient(at 91% 83%, hsla(283, 74%, 69%, 1) 0px, transparent 50%),
-    radial-gradient(at 72% 91%, hsla(213, 75%, 75%, 1) 0px, transparent 50%);
+body {
+  background: #f1f1f1;
+  height: 100vh;
 }
-
-.input__button__shadow {
-  cursor: pointer;
-  border: none;
-  background: none;
-  -webkit-transition:
-    background 400ms,
-    -webkit-transform 400ms;
-  transition:
-    background 400ms,
-    -webkit-transform 400ms;
-  transition:
-    transform 400ms,
-    background 400ms;
-  transition:
-    transform 400ms,
-    background 400ms,
-    -webkit-transform 400ms;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  justify-content: center;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-  border-radius: 12px;
-  padding: 5px;
-}
-
-.input__button__shadow:hover {
-  background: rgba(255, 255, 255, 0.411);
-}
-
-.input__search {
-  width: 100%;
-  border-radius: 20px;
-  outline: none;
-  border: none;
-  padding: 8px;
-  position: relative;
+.search-bar input,
+.search-btn,
+.search-btn:before,
+.search-btn:after {
+  transition: all 0.25s ease-out;
 }
 .resultsSuggested {
-  margin-top: 1rem;
-  width: 300px;
-  padding: 1rem;
   background: white;
+  width: calc(100% - 3em);
+  margin-inline: 2px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  text-align: center;
+  overflow: hidden;
+}
+
+.searchAndResults {
+  width: min(80%, 30em);
+}
+.search-bar input,
+.search-btn {
+  width: 3em;
+  height: 3em;
+}
+.search-bar input:invalid:not(:focus),
+.search-btn {
+  cursor: pointer;
+}
+.search-bar,
+.search-bar input:focus,
+.search-bar input:valid {
+  width: 100%;
+}
+.search-bar input:focus,
+.search-bar input:not(:focus) + .search-btn:focus {
+  outline: transparent;
+}
+.search-bar {
+  margin: auto;
+  justify-content: center;
+}
+.search-bar input {
+  background: transparent;
+  border-radius: 1.5em;
+  box-shadow: 0 0 0 0.4em #171717 inset;
+  padding: 0.75em;
+  transform: translate(0.5em, 0.5em) scale(0.5);
+  transform-origin: 100% 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+.search-bar input::-webkit-search-decoration {
+  -webkit-appearance: none;
+}
+.search-bar input:focus,
+.search-bar input:valid {
+  background: #fff;
+  border-radius: 0.375em 0 0 0.375em;
+  box-shadow: 0 0 0 0.1em #d9d9d9 inset;
+  transform: scale(1);
+}
+.search-btn {
+  background: #171717;
+  border-radius: 0 0.75em 0.75em 0 / 0 1.5em 1.5em 0;
+  padding: 0.75em;
+  position: relative;
+  transform: translate(0.25em, 0.25em) rotate(45deg) scale(0.25, 0.125);
+  transform-origin: 0 50%;
+}
+.search-btn:before,
+.search-btn:after {
+  content: '';
+  display: block;
+  opacity: 0;
+  position: absolute;
+}
+.search-btn:before {
+  border-radius: 50%;
+  box-shadow: 0 0 0 0.2em #f1f1f1 inset;
+  top: 0.75em;
+  left: 0.75em;
+  width: 1.2em;
+  height: 1.2em;
+}
+
+.suggested {
+  border-bottom: 1px solid #b5a2a2ad;
+  padding-inline: 1rem;
+  transition: 0.3s ease-in-out;
+  cursor: pointer;
+}
+.suggested:hover {
+  background: rgb(0, 0, 0, 0.03);
+}
+.empty {
+  padding-block: 0.5rem;
+}
+.search-btn:after {
+  background: #f1f1f1;
+  border-radius: 0 0.25em 0.25em 0;
+  top: 51%;
+  left: 51%;
+  width: 0.75em;
+  height: 0.25em;
+  transform: translate(0.2em, 0) rotate(45deg);
+  transform-origin: 0 50%;
+}
+.search-btn span {
+  display: inline-block;
+  overflow: hidden;
+  width: 1px;
+  height: 1px;
+}
+
+/* Active state */
+.search-bar input:focus + .search-btn,
+.search-bar input:valid + .search-btn {
+  background: #2762f3;
+  border-radius: 0 0.375em 0.375em 0;
+  transform: scale(1);
+}
+.search-bar input:focus + .search-btn:before,
+.search-bar input:focus + .search-btn:after,
+.search-bar input:valid + .search-btn:before,
+.search-bar input:valid + .search-btn:after {
+  opacity: 1;
+}
+.search-bar input:focus + .search-btn:hover,
+.search-bar input:valid + .search-btn:hover,
+.search-bar input:valid:not(:focus) + .search-btn:focus {
+  background: #0c48db;
+}
+.search-bar input:focus + .search-btn:active,
+.search-bar input:valid + .search-btn:active {
+  transform: translateY(1px);
+}
+
+@media screen and (prefers-color-scheme: dark) {
+  body,
+  input {
+    color: #f1f1f1;
+  }
+  body {
+    background: #171717;
+  }
+  .search-bar input {
+    box-shadow: 0 0 0 0.4em #f1f1f1 inset;
+  }
+  .search-bar input:focus,
+  .search-bar input:valid {
+    background: #3d3d3d;
+    box-shadow: 0 0 0 0.1em #3d3d3d inset;
+  }
+  .search-btn {
+    background: #f1f1f1;
+  }
 }
 </style>
