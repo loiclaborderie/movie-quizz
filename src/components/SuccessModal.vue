cfb3c4cd90f3e86b0a9d53c1dd8b82a9191c9aac
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2023-11-14 12:00:36 +0700

    chore: added modal when movie is found

---
+++ src/components/SuccessModal.vue
@@ -0,0 +1,90 @@
+<template>
+  <AppModal>
+    <template #title>{{ congrats }}</template>
+    <template #content>
+      <div class="tries">
+        Tu as réussi en {{ attempts.length }} {{ attempts.length > 1 ? 'essais' : 'essai' }}. Le
+        film était bien :
+      </div>
+      <div class="movieFound">
+        <MovieCard :movie="movie" />
+      </div>
+    </template>
+    <template #actions>
+      <div class="buttons">
+        <button @click="emit('reset')" class="action-btn black">New Game</button>
+      </div>
+    </template>
+  </AppModal>
+</template>
+
+<script setup lang="ts">
+import type MovieDetails from '@/types/MovieDetails'
+import AppModal from '@/components/shared/AppModal.vue'
+import { computed } from 'vue'
+import MovieCard from '@/components/MovieCard.vue'
+import { movieGuessFinderStore } from '@/stores/movieGuessFinder'
+interface SuccessModal {
+  attempts: MovieDetails[]
+  movie: MovieDetails
+}
+const props = defineProps<SuccessModal>()
+const store = movieGuessFinderStore()
+const emit = defineEmits(['reset'])
+
+const congrats = computed(() => {
+  const numberTries = props.attempts.length
+  if (numberTries === 0) {
+    return 'WOW, quel génie !'
+  } else if (numberTries < 5) {
+    return 'Bravo, quelle performance !'
+  } else if (numberTries < 10) {
+    return 'Pas mal !'
+  } else if (numberTries < 15) {
+    return 'Tu peux mieux faire...'
+  } else {
+    return 'Bel effort mais insuffisant'
+  }
+})
+</script>
+
+<style scoped>
+.tries {
+  text-align: center;
+}
+
+.movieFound {
+  max-width: 600px;
+}
+
+::v-deep .movie-card {
+  width: 100%;
+}
+
+.buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 3rem;
+}
+
+.action-btn {
+  padding: 0.45rem 1.2rem;
+  border: 1px solid #e7e7e7;
+  background: transparent;
+  border-radius: 7px;
+  cursor: pointer;
+  transition: 0.3s ease;
+}
+.action-btn:hover {
+  background: rgb(238, 238, 238);
+}
+.action-btn.black:hover {
+  background: rgb(41, 41, 41);
+}
+.action-btn.black {
+  border: 1px solid #e7e7e7;
+  background: rgb(25, 25, 25);
+  color: #f1f1f1;
+}
+</style>
