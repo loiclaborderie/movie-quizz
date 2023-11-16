ca2a8cbced511103023e7b684f0cf2d833f5ce13
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2023-11-16 11:58:58 +0700

    Refactor movieGuessFinderStore to improve code readability and maintainability.

--- src/stores/movieGuessFinder.ts
+++ src/stores/movieGuessFinder.ts
@@ -3,46 +3,44 @@ import { defineStore } from 'pinia'
 import type MovieDetails from '@/types/MovieDetails'
 import { useMovieApi } from '@/composables/useMovieApi'
 
-interface foundHints {
+interface Interval {
+  moreThan: number | null
+  lessThan: number | null
+}
+
+interface FoundHints {
   year: Interval
   movieLength: Interval
 }
-interface foundClues {
+
+interface FoundClues {
   year: number | null
   movieLength: number | null
   genre: string[]
-}
-interface Interval {
-  moreThan: null | number
-  lessThan: null | number
 }
 
 export const movieGuessFinderStore = defineStore('movieGuessFinder', () => {
   const movieToFind = ref<MovieDetails | null>(null)
   const failedAttempts = ref<MovieDetails[]>([])
 
-  const hintsFound = ref<foundHints>({
+  const hintsFound = ref<FoundHints>({
     year: { moreThan: null, lessThan: null },
     movieLength: { moreThan: null, lessThan: null }
   })
-  const cluesFound = reactive<foundClues>({ year: null, genre: [], movieLength: null })
+
+  const cluesFound = reactive<FoundClues>({ year: null, genre: [], movieLength: null })
+
   const { getMovieById } = useMovieApi()
 
-  function compareMovie(movieId: number) {
+  function compareMovie(movieId: number): boolean {
     if (!movieToFind.value) {
-      console.error('there has been an error somewhere bro')
-      return
-    }
-
-    console.log(movieId, movieToFind.value.id, movieId === movieToFind.value.id)
-    if (movieId === movieToFind.value.id) {
-      alert('you found the right movie')
-      return true
-    } else {
-      alert('try again, youre getting closer')
-      // getHints(movieId)
+      console.error('There has been an error somewhere.')
       return false
     }
+
+    const isMatch = movieId === movieToFind.value.id
+    console.log(`Movie ID ${movieId} ${isMatch ? 'matches' : 'does not match'} the target movie ID ${movieToFind.value.id}.`)
+    return isMatch
   }
 
   function resetGame() {
@@ -56,9 +54,10 @@ export const movieGuessFinderStore = defineStore('movieGuessFinder', () => {
     failedAttempts.value.unshift(comparedMovie)
 
     if (!movieToFind.value || !compareMovie) {
-      console.error('there has been an error somewhere bro')
+      console.error('There has been an error somewhere.')
       return null
     }
+
     const yearFound = parseInt(comparedMovie.release_date.slice(0, 4))
     const yearToMatch = parseInt(movieToFind.value.release_date.slice(0, 4))
     const lengthFound = comparedMovie.runtime
@@ -66,32 +65,31 @@ export const movieGuessFinderStore = defineStore('movieGuessFinder', () => {
     const genresFound = comparedMovie.genres.map((genre) => genre.name)
     const genresToMatch = movieToFind.value.genres.map((genre) => genre.name)
 
-    console.log(`year found: ${yearFound}, year to match : ${yearToMatch}`)
+    console.log(`Year found: ${yearFound}, year to match: ${yearToMatch}`)
+
+    const yearInterval = hintsFound.value.year
+    const lengthInterval = hintsFound.value.movieLength
 
     if (!cluesFound.year) {
       if (yearFound === yearToMatch) {
         cluesFound.year = yearToMatch
-        console.log('congrats, you found the right year !')
+        console.log('Congratulations, you found the right year!')
       } else {
-        if (!isNaN(yearToMatch) && yearToMatch !== 0 && yearFound !== 0) {
-          guessNumber(yearFound, yearToMatch, hintsFound.value.year)
-        }
+        guessNumber(yearFound, yearToMatch, yearInterval)
       }
-      console.log(`hintsFound = `, hintsFound.value.year)
-      console.log(`cluesFound = `, cluesFound.year)
+      console.log(`Hints found: `, yearInterval)
+      console.log(`Clues found: `, cluesFound.year)
     }
 
     if (!cluesFound.movieLength) {
       if (lengthFound === lengthToMatch) {
         cluesFound.movieLength = lengthToMatch
-        console.log('congrats, you found the right length')
+        console.log('Congratulations, you found the right length!')
       } else {
-        if (!isNaN(lengthToMatch) && lengthToMatch !== 0 && lengthFound !== 0) {
-          guessNumber(lengthFound, lengthToMatch, hintsFound.value.movieLength)
-        }
+        guessNumber(lengthFound, lengthToMatch, lengthInterval)
       }
-      console.log(`hintsFound = `, hintsFound.value.movieLength)
-      console.log(`cluesFound = `, cluesFound.movieLength)
+      console.log(`Hints found: `, lengthInterval)
+      console.log(`Clues found: `, cluesFound.movieLength)
     }
 
     genresFound.forEach((genre) => {
@@ -104,51 +102,13 @@ export const movieGuessFinderStore = defineStore('movieGuessFinder', () => {
   }
 
   function guessNumber(numberTrial: number, numberToFind: number, interval: Interval) {
-    const numberDiff = (number: number) => number - numberToFind
-    const foundDiff = numberDiff(numberTrial)
-    console.log('important', numberTrial, numberDiff)
+    const foundDiff = numberTrial - numberToFind
 
     if (foundDiff > 0) {
-      if (!interval.lessThan) {
-        interval.lessThan = numberTrial
-      } else {
-        const numberToKeep = Math.min(interval.lessThan, numberTrial)
-        interval.lessThan = numberToKeep
-      }
+      interval.lessThan = !interval.lessThan ? numberTrial : Math.min(interval.lessThan, numberTrial)
     } else {
-      if (!interval.moreThan) {
-        interval.moreThan = numberTrial
-      } else {
-        const numberToKeep = Math.max(interval.moreThan, numberTrial)
-        interval.moreThan = numberToKeep
-      }
+      interval.moreThan = !interval.moreThan ? numberTrial : Math.max(interval.moreThan, numberTrial)
     }
-
-    // if (!interval.moreThan && !interval.lessThan) {
-    //   if (foundDiff > 0) {
-    //     interval.lessThan = numberTrial
-    //   }
-    // } else if (interval.moreThan) {
-    //   if (numberTrial < numberToFind) {
-    //     interval.moreThan = numberTrial
-    //   }
-    // } else if (interval.lessThan) {
-    //   if (numberTrial > numberToFind) {
-    //     interval.lessThan = numberTrial
-    //   }
-    // } else {
-    //   if (foundDiff > 0 && interval.lessThan) {
-    //     console.log('condition truc bizarre1')
-    //     const numberToKeep = Math.min(interval.lessThan, numberTrial)
-    //     interval.lessThan = numberToKeep
-    //   } else if (foundDiff < 0 && interval.moreThan) {
-    //     console.log('condition truc bizarre2')
-    //     const numberToKeep = Math.max(interval.moreThan, numberTrial)
-    //     interval.moreThan = numberToKeep
-    //   }
-    // }
-
-    console.log(interval)
   }
 
   return { movieToFind, compareMovie, getHints, hintsFound, cluesFound, failedAttempts, resetGame }
