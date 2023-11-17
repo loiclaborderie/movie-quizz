09c24f0c136620740329bf51c65c34cdd33b0845
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2023-11-17 11:34:52 +0700

    chore: add package and icon

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faRotateRight } from '@fortawesome/free-solid-svg-icons';
+
 
 import App from './App.vue'
 import router from './router'
+library.add(faRotateRight);
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 
 app.mount('#app')
